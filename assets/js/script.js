9a062faabf3860de7c103f02fde3af5dca2d7299
Dxnale <daniel.torrealba@alumnos.ipleones.cl>
2024-05-31 01:39:44 -0400

    subida

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('back-to-top').addEventListener('click', () => {
+    document.documentElement.scrollTop = 0;
+});
+
+document.getElementById('info-btn').addEventListener('click', () => {
+    document.documentElement.scrollTop = 630;
+});
+
+document.getElementById('play-btn').addEventListener('click', () => {
+    document.documentElement.scrollTop = 1500;
+});
